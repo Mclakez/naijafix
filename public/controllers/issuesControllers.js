a88316491f59ed6cc0dfc465310738e57ba9cd60
Fadairo Marvellous Ayokunle <mafadairo@student.lautech.edu.ng>
2025-10-08 12:56:20 +0100

    homepage start

--- public/controllers/issuesControllers.js
+++ public/controllers/issuesControllers.js
@@ -46,13 +46,18 @@ export async function updateIssuesStatus(req, res) {
     let db = await getDB()
     let {id} = req.params
     let {status} = req.body
+    const proofImage = req.file ? req.file.filename: null
     
     const validStatuses = ["acknowledged", "in-progress", "resolved"]
    
     if(!validStatuses.includes(status)) return res.status(400).json({error: "Invalid status"})
+    if (status === "resolved" && !proofImage) return res.status(400).json({error: "Proof image is required when status is 'resolved'"})
     
     try {
-        await db.run(`UPDATE issues SET status = ? WHERE id = ?`, [status, id])
+        await db.run(`UPDATE issues
+             SET status = COALESCE(?, status),
+                 proofImage = COALESCE(?, status)
+              WHERE id = ?`, [status, proofImage, id])
         const updated = await db.get(`SELECT * FROM issues WHERE id = ?`, [id])
         res.json(updated)
     } catch (err) {
@@ -60,20 +65,20 @@ export async function updateIssuesStatus(req, res) {
     }
 }
 
-export async function addFixPhoto(req, res) {
-    let db = await getDB()
-    const { id } = req.params
-    const proofImage = req.file ? req.file.filename: null
-    try {
-        await db.run(`
-            UPDATE issues SET proofImage = ? WHERE id = ?
-            `, [proofImage, id])
+// export async function addFixPhoto(req, res) {
+//     let db = await getDB()
+//     const { id } = req.params
+//     const proofImage = req.file ? req.file.filename: null
+//     try {
+//         await db.run(`
+//             UPDATE issues SET proofImage = ? WHERE id = ?
+//             `, [proofImage, id])
 
-            const updated = await db.get(`
-                SELECT * FROM issues WHERE id = ?
-                `, [id])
-                res.json(updated)
-    } catch (err) {
-        res.status(500).json({error: err.message})
-    }
-}
+//             const updated = await db.get(`
+//                 SELECT * FROM issues WHERE id = ?
+//                 `, [id])
+//                 res.json(updated)
+//     } catch (err) {
+//         res.status(500).json({error: err.message})
+//     }
+// }
--- public/routes/issuesRoutes.js
+++ public/routes/issuesRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { postIssue, getMyIssues, getAllIssues, updateIssuesStatus, addFixPhoto } from '../controllers/issuesControllers.js'
+import { postIssue, getMyIssues, getAllIssues, updateIssuesStatus} from '../controllers/issuesControllers.js'
 import { requireAuth, requireOfficer } from '../middleware/auth.js'
 import { upload } from "../config/multer.js";
 import { issuesValidator } from "../validators/issuesValidator.js";
@@ -10,6 +10,6 @@ export const issuesRouter = express.Router()
 issuesRouter.post('/', issuesValidator, requireAuth, upload.single('image'), postIssue)
 issuesRouter.get('/', requireAuth, getAllIssues)
 issuesRouter.get('/my', requireAuth, getMyIssues)
-issuesRouter.patch('/:id/status', requireAuth, requireOfficer, updateIssuesStatus)
+issuesRouter.patch('/:id/status', requireAuth, requireOfficer, upload.single('proof'),updateIssuesStatus)
 
-issuesRouter.patch('/:id/fix-photo', requireAuth, requireOfficer, upload.single('proof'), addFixPhoto)
+// issuesRouter.patch('/:id/fix-photo', requireAuth, requireOfficer,, addFixPhoto)
